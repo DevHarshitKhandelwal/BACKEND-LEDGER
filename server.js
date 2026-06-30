878fab1b35eaff5735bd95cfe1d8dcccca3667fd
HyIAmHarshit <khandelwalharshit2024@gmail.com>
2026-06-30 10:29:03 +0530

    updateee

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connectToDb=require("./src/config/db.js")
 connectToDb();
 
 
-const PORT = 3000;
-app.listen(PORT,()=>{
+
+app.listen(3000,()=>{
     console.log(`server is running on PORT: ${3000}`)
 })
